2b87e73be062d25a1844a5168c2df03de317dcfb
mansi <mansi.chourasia@cgi.com>
2019-07-11 11:34:13 +0530

    updated assignment1 of checking palindrom

--- src/main/java/com/stackroot/Assignment1.java
+++ src/main/java/com/stackroot/Assignment1.java
@@ -1,3 +1,5 @@
+package com.stackroot;
+
 import  java.util.Scanner;
 class Assignment1{
     public static void main(String args[]){
@@ -19,9 +21,9 @@ class Assignment1{
         }
         if((temp==sum)  && (sum1>25))
             System.out.println("the given number"+temp+"palindrome number and greater than 25 ");
-       if ((temp==sum)  && (sum1<25))
+        if ((temp==sum)  && (sum1<25))
             System.out.println("the given number"+temp+"not palindrome and less than 25");
-       if(temp !=sum)
-       System.out.println("not palindrome");
+        if(temp !=sum)
+            System.out.println("not palindrome");
     }
 }
