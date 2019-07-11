b389c6ea1fc84310830f576e3d4ae032e2ea4498
mansi <mansi.chourasia@cgi.com>
2019-07-11 11:28:00 +0530

    updated README.md file and practiceAssignment10

--- src/main/java/com/stackroot/Assignment10.java
+++ src/main/java/com/stackroot/Assignment10.java
@@ -1,4 +1,22 @@
 package com.stackroot;
 
-public class Assignment10 {
+import java.util.*;
+
+class Assignment10 {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.next();
+        System.out.println("enter a string");
+        int n = sc.nextInt();
+        String result="";
+        System.out.println("enter a number");
+        String subString = str1.substring(n);
+        for (int i = 0; i < n; i++) {
+
+          result=result+subString;
+        }
+        System.out.println(str1+result);
+    }
+
 }
